d98ef8acd02316d1f8e835cc8144861fe9e638df
Jing Guo <dev.guoj@gmail.com>
2021-12-07 20:14:06 +0800

    solved!

---
+++ 7.lisp
@@ -0,0 +1,25 @@
+(defparameter *crab-pos* (uiop:read-file-line "input/day7.txt"))
+
+(defun crab-pos-list (crabs)
+  (mapcar #'parse-integer (uiop:split-string crabs :separator ",")))
+
+(defun difference (n crab)
+  (abs (- n crab)))
+
+(defun gauss-summ (n crab)
+  (let ((num (difference n crab)))
+    (/ (* (+ 1 num) num) 2)))
+
+(defun sum (lst)
+  (reduce #'+ lst))
+
+(defun solve-first-part (crabs)
+  (let* ((sorted-crabs (stable-sort (crab-pos-list crabs) #'<=))
+         (min-crab (first sorted-crabs))
+         (max-crab (first (last sorted-crabs))))
+    (loop for i from min-crab to max-crab
+          minimize (sum (mapcar (lambda (n)
+                                  ;; first part is `difference`
+                                  ;; second part is `gauss-sum`
+                                  (gauss-sum n i))
+                                sorted-crabs)))))
